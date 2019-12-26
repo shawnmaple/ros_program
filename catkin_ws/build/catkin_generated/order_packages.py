4aafbd5667162499ba3c0626203df6d23b69d3d3
shawnmaple <18829354118@163.com>
2019-12-26 14:36:40 +0800

    first ros commit

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/shawn/workspace/ros_program/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/shawn/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/shawn/catkin_ws/devel;/opt/ros/kinetic" != "" else []
---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/shawn/workspace/ros_program/catkin_ws/devel:/home/shawn/catkin_ws/devel:/opt/ros/kinetic"
+export LD_LIBRARY_PATH="/home/shawn/catkin_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
+export PWD="/home/shawn/workspace/ros_program/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/shawn/workspace/ros_program/catkin_ws/devel/share/common-lisp:/home/shawn/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/shawn/workspace/ros_program/catkin_ws/src:/home/shawn/catkin_ws/src:/opt/ros/kinetic/share"
